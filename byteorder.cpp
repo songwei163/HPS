7ea5253779bf332fcbb1bf40e6a224fa86821239
songwei163 <songweicpp@163.com>
2019-05-02 12:27:19 +0800

    测试大小端

---
+++ byteorder.cpp
@@ -0,0 +1,35 @@
+//
+// Created by s on 19-5-2.
+//
+/*
+ * 判断大小端
+ */
+#include <cstdio>
+
+void byteorder ()
+{
+  union {
+    short value;
+    char union_bytes[sizeof (short)];
+  } test;
+
+  test.value = 0x0102;
+  if ((test.union_bytes[0] == 1) && (test.union_bytes[1] == 2))
+    {
+      printf ("big endian.\n");
+    }
+  else if ((test.union_bytes[0] == 2) && (test.union_bytes[1] == 1))
+    {
+      printf ("little endian.\n");
+    }
+  else
+    {
+      printf ("unkonwn.\n");
+    }
+}
+
+int main ()
+{
+  byteorder ();
+  return 0;
+}
